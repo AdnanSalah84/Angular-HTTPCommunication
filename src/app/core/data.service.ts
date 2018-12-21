21b01037a7d301d366c709f26d562a185f9fe9e3
adnansalah84 <adnan.salah84@gmail.com>
2018-12-21 14:41:47 +0500

    created advance http requests and error handling

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, catchError } from 'rxjs/operators';
 
 import { allBooks, allReaders } from 'app/data';
 import { Reader } from "app/models/reader";
 import { Book } from "app/models/book";
 import { BookTrackerError } from 'app/models/bookTrackerError';
 import { OldBook } from 'app/models/oldBook';
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
 
 @Injectable()
 export class DataService {
@@ -30,9 +31,20 @@ export class DataService {
     return allReaders.find(reader => reader.readerID === id);
   }
 
-  getAllBooks(): Observable<Book[]> {
+  getAllBooks(): Observable<Book[] | BookTrackerError> {
     console.log('Getting all books from the server.');
-    return this.http.get<Book[]>('/api/books');
+    return this.http.get<Book[]>('/api/books')
+      .pipe(
+        catchError(err => this.handleHttpError(err))
+      );
+  }
+
+   private handleHttpError(error: HttpErrorResponse) : Observable<BookTrackerError> {
+     let dataError = new BookTrackerError();
+     dataError.errorNumber = 100;
+     //dataError.message = error.statusText;
+     dataError.friendlyMessage = 'An error occurred retrieving data.';
+     return ErrorObservable.create(dataError);
   }
 
   getBookById(id: number): Observable<Book> {
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit, VERSION } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { Title } from '@angular/platform-browser';
@@ -5,6 +6,7 @@ import { Title } from '@angular/platform-browser';
 import { Book } from "app/models/book";
 import { Reader } from "app/models/reader";
 import { DataService } from 'app/core/data.service';
+import { BookTrackerError } from '../models/bookTrackerError';
 
 @Component({
   selector: 'app-dashboard',
@@ -18,15 +20,23 @@ export class DashboardComponent implements OnInit {
   mostPopularBook: Book;
 
   constructor(private dataService: DataService,
-              private title: Title) { }
-  
+              private title: Title,
+              private route: ActivatedRoute) { }
+
   ngOnInit() {
-    this.dataService.getAllBooks()
-      .subscribe(
-        (data: Book[]) => this.allBooks = data,
-        (err: any) => console.log(err),
-        () => console.log('All done getting books.')
-      );
+    let resolvedData: Book[] | BookTrackerError = this.route.snapshot.data['resolvedBooks'];
+
+    if(resolvedData instanceof BookTrackerError){
+      console.log(`Dashboard component error: ${resolvedData.friendlyMessage}`);
+    } else {
+      this.allBooks = resolvedData;
+    }
+    // this.dataService.getAllBooks()
+    //   .subscribe(
+    //     (data: Book[]) => this.allBooks = data,
+    //     (err: BookTrackerError) => console.log(err.friendlyMessage),
+    //     () => console.log('All done getting books.')
+    // );
     this.allReaders = this.dataService.getAllReaders();
     this.mostPopularBook = this.dataService.mostPopularBook;
 
@@ -41,7 +51,7 @@ export class DashboardComponent implements OnInit {
           this.allBooks.splice(index, 1);
         },
         (err: any) => console.log(err)
-      );    
+      );
   }
 
   deleteReader(readerID: number): void {
